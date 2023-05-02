f86fda8685396f7ee7ea61e02087dd14fe16ca9c
VadymMakovii <makovii.vadym@gmail.com>
2023-05-02 12:23:58 +0300

    Changed backgroundColor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,15 @@ export const App = () => {
       style={{
         width: '100%',
         height: '100%',
-        backgroundColor: ' green',
+        background: "linear-gradient(114.99deg,#471ca9 -0.99%,#5736a3 54.28%,#4b2a99 78.99%)",
+        color: "#FFFFFF80",
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         minHeight: '100vh',
       }}
     >
-      <h1>This is my own template!!!</h1>
+      <h1>This is my own template !!!</h1>
     </div>
   );
 };
